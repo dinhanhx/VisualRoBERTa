0ff1c911cf24c75a55fdb574ac29f6e53f486315
dinhanhx <dinhanhx@gmail.com>
2022-08-29 14:18:59 +0000

    Update install scripts

--- sh_scripts/README.md
+++ sh_scripts/README.md
@@ -7,3 +7,10 @@ export XRT_TPU_CONFIG="localservice;0;localhost:51011"
 export PT_XLA_DEBUG=1
 export USE_TORCH=ON
 ```
+
+`env_setup.sh` to setup a conda environment which has
+- torch with tpu support
+- torchvision
+- transformer[torch]
+
+`install_cmds.sh` like `env_setup.sh` but just to do `pip install`
--- sh_scripts/env_setup.sh
+++ sh_scripts/env_setup.sh
@@ -1,5 +1,6 @@
 conda create --name torch-tpu python=3.8 pip
 conda activate torch-tpu
-pip3 install torch==1.11.0 https://storage.googleapis.com/tpu-pytorch/wheels/tpuvm/torch_xla-1.11-cp38-cp38-linux_x86_64.whl
+pip3 install torch==1.11.0
+pip3 install torchvision==0.12.0
+pip3 install https://storage.googleapis.com/tpu-pytorch/wheels/tpuvm/torch_xla-1.11-cp38-cp38-linux_x86_64.whl
 pip3 install torch_xla[tpuvm]
-pip3 install transformers[torch]
