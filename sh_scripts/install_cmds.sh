f5c3f2fa9ad127b98ffc6992afa5929615c97738
dinhanhx <dinhanhx@gmail.com>
2022-09-15 10:10:01 +0000

    Update setup scripts

--- sh_scripts/install_cmds.sh
+++ sh_scripts/install_cmds.sh
@@ -2,3 +2,7 @@ pip3 install torch==1.11.0
 pip3 install torchvision==0.12.0
 pip3 install https://storage.googleapis.com/tpu-pytorch/wheels/tpuvm/torch_xla-1.11-cp38-cp38-linux_x86_64.whl
 pip3 install torch_xla[tpuvm]
+pip3 install pandas
+pip3 install einops
+pip3 install sentencepiece
+pip3 install transformers[torch]
