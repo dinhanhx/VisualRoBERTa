767dd31abe4a96dfe7fcee72cbf7f5279aedaf0c
dinhanhx <dinhanhx@gmail.com>
2022-09-29 15:04:05 +0000

    Refactor

--- eval_uit_viic.py
+++ eval_uit_viic.py
@@ -1,43 +1,16 @@
 from src.data import ImageTextPair
 from src.tokenization import BunTokenizer
 from src.vision_language import ImageTextForCausalLM, ImageTextConfig
+from src.utils import ImageCaptioningTools
 from pathlib import Path
 from tqdm import tqdm
 import torch
-from torchvision.io import read_image, ImageReadMode
-from torchvision.transforms.functional import resize
-from transformers.tokenization_utils_base import BatchEncoding
 import json
+from typing import Callable
 
 
-def process_raw_data(dp, tokenizer: BunTokenizer, image_size: list, patch_size: list):
-    """ To process a data point from IC dataloader
-    for the inference phase of ImageTextCasualLM
-    """
-    text_inputs = tokenizer(' ', return_tensors='pt')
-    image_inputs = torch.stack([resize(read_image(str(dp['img_file']),
-                                                  ImageReadMode.RGB),
-                                       image_size)],
-                               0).float()
-    num_patches = (image_size[0] // patch_size[0]) * (image_size[1] // patch_size[1])
-
-    # Extend the shape of text_inputs.attention_masks to cover image_inputs
-    extra_attention_mask = torch.ones(1, num_patches, dtype=text_inputs.attention_mask.dtype)
-    attention_mask = torch.cat((text_inputs.attention_mask[:,:-1], extra_attention_mask), dim=1)  # [:,:-1] to ignore [SEP] token to enable sentence completion
-
-    return BatchEncoding({'input_ids': text_inputs.input_ids[:,:-1],  # [:,:-1] to ignore [SEP] token to enable sentence completion
-                          'attention_mask': attention_mask,
-                          'token_type_ids': text_inputs.token_type_ids[:,:-1],  # [:,:-1] to ignore [SEP] token to enable sentence completion
-                          'image_input': image_inputs})
-
-
-def prettify_output(output, tokenizer: BunTokenizer, return_str: bool = True):
-    first_sep_index = (output[0] == 4).nonzero(as_tuple=True)[0][0]
-    caption = output[0][1: first_sep_index]
-    if return_str:
-        return tokenizer.decode(caption)
-    else:
-        return caption
+process_raw_data: Callable = ImageCaptioningTools.process_raw_data
+prettify_output: Callable = ImageCaptioningTools.prettify_output
 
 
 ic = ImageTextPair(Path('/home/dinhanhx/data/'),
@@ -48,7 +21,7 @@ bun_tokenizer = BunTokenizer.from_pretrained('vinai/bartpho-syllable')
 
 ver = 1
 config = ImageTextConfig.from_json_file('assets/imagetext-casual-base-config.json')
-model = ImageTextForCausalLM(config)
+model = ImageTextForCausalLM(config)  # type: ignore
 model.load_state_dict(torch.load(f'uit_viic_logs/lightning_logs/version_{ver}/checkpoints/uit_viic.pt'))
 model = model.eval()
 
@@ -64,7 +37,7 @@ for i in tqdm(range(lic)):
         cur_image_id = dp['image_id']
         inputs = process_raw_data(dp, bun_tokenizer, config.image_size, config.patch_size)
         with torch.no_grad():
-            output = model.generate(inputs=inputs['input_ids'],
+            output = model.generate(inputs=inputs['input_ids'],  # type: ignore
                                     max_new_tokens=40,
                                     num_beams=5,
                                     do_sample=False,
@@ -74,8 +47,8 @@ for i in tqdm(range(lic)):
                                     image_input=inputs['image_input'])
             predict = prettify_output(output, bun_tokenizer)
             d[f'{counter}'] = {'predict': predict,
-                            'image_id': dp['image_id']}
+                               'image_id': dp['image_id']}
             counter += 1
 
-with open('test_uit_viic.json', 'w', encoding='utf-8') as tf:
+with open('assets/test_uit_viic.json', 'w', encoding='utf-8') as tf:
     json.dump(d, tf, ensure_ascii=False)
--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,10 @@
 import torch
+from torchvision.io import read_image, ImageReadMode
+from torchvision.transforms.functional import resize
 from transformers.tokenization_utils_base import BatchEncoding
 
 from src.vision_language import ImageTextConfig
+from src.tokenization import BunTokenizer
 
 
 def prepare_vl_inputs(text_inputs: BatchEncoding, image_input: torch.Tensor, config: ImageTextConfig):
@@ -17,3 +20,44 @@ def prepare_vl_inputs(text_inputs: BatchEncoding, image_input: torch.Tensor, con
     text_inputs.data['attention_mask'] = torch.cat((text_inputs.attention_mask, extra_attention_masks), dim=1)  # type: ignore
 
     return text_inputs
+
+
+class ImageCaptioningTools:
+    """Tools for pre-process and post-forward
+    for inference mode of ImageTextCausalLM
+    """
+
+    @staticmethod
+    def process_raw_data(dp, tokenizer: BunTokenizer, image_size: list, patch_size: list):
+        """ To process a data point from IC dataloader
+        for the inference phase of ImageTextCasualLM
+        """
+        text_inputs = tokenizer(' ', return_tensors='pt')
+        image_inputs = torch.stack([resize(read_image(str(dp['img_file']),
+                                                      ImageReadMode.RGB),
+                                           image_size)],
+                                   0).float()
+        num_patches = (image_size[0] // patch_size[0]) * (image_size[1] // patch_size[1])
+
+        # Extend the shape of text_inputs.attention_masks to cover image_inputs
+        extra_attention_mask = torch.ones(1, num_patches, dtype=text_inputs.attention_mask.dtype)
+        # [:,:-1] to ignore [SEP] token to enable sentence completion
+        attention_mask = torch.cat((text_inputs.attention_mask[:, :-1], extra_attention_mask), dim=1)
+
+        return BatchEncoding({'input_ids': text_inputs.input_ids[:, :-1],
+                              'attention_mask': attention_mask,
+                              'token_type_ids': text_inputs.token_type_ids[:, :-1],
+                              'image_input': image_inputs})
+
+    @staticmethod
+    def prettify_output(output, tokenizer: BunTokenizer, return_str: bool = True):
+        """ To remove everything that is behind [SEP]
+        """
+        first_sep_index = (output[0] == tokenizer.sep_token_id).nonzero(as_tuple=True)[0][0]
+        caption = output[0][1: first_sep_index]
+        if return_str:
+            return tokenizer.decode(caption)
+        else:
+            return caption
+
+
