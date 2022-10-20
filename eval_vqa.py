cdec83f651472129618ae24a0a4f25b14e69052d
dinhanhx <dinhanhx@gmail.com>
2022-10-20 04:15:34 +0000

    Remove unused import, fix hardcoded path

--- eval_vqa.py
+++ eval_vqa.py
@@ -1,4 +1,4 @@
-from src.data import VisualQuestionAnswer, VisualQuestionAnswerCollator
+from src.data import VisualQuestionAnswer
 from src.tokenization import BunTokenizer
 from src.vision_language import ImageTextForCausalLM, ImageTextConfig
 from src.utils import VisualQuestionAnswerTools
@@ -20,7 +20,7 @@ bun_tokenizer = BunTokenizer.from_pretrained('vinai/bartpho-syllable')
 
 ver = 1
 config = ImageTextConfig.from_json_file('assets/imagetext-casual-base-config.json')
-model = ImageTextForCausalLM(config)
+model = ImageTextForCausalLM(config)  # type: ignore
 model.load_state_dict(torch.load(f'vqa_logs/lightning_logs/version_{ver}/checkpoints/vqa.pt'))
 model = model.eval()
 
@@ -30,7 +30,7 @@ for i in tqdm(range(ldp)):
     dp = vqa[i]
     inputs = process_raw_data(dp, bun_tokenizer, config.image_size, config.patch_size)
     with torch.no_grad():
-        output = model.generate(inputs=inputs['input_ids'],
+        output = model.generate(inputs=inputs['input_ids'],  # type: ignore
                                 max_new_tokens=10,
                                 num_beams=5,
                                 do_sample=False,
@@ -39,9 +39,9 @@ for i in tqdm(range(ldp)):
                                 token_type_ids=inputs['token_type_ids'],
                                 image_input=inputs['image_input'])
     predict = prettify_output(output, bun_tokenizer)
-    reference: str = dp['answer']
+    reference: str = dp['answer']  # type: ignore
     correct = None
-    if predict.lower() == reference.lower():
+    if predict.lower() == reference.lower():  # type: ignore
         correct = True
     else:
         correct = False
@@ -50,5 +50,5 @@ for i in tqdm(range(ldp)):
                  'predict': predict,
                  'reference': reference}
 
-with open('test_vqa.json', 'w', encoding='utf-8') as tf:
+with open('assets/test_vqa.json', 'w', encoding='utf-8') as tf:
     json.dump(d, tf, ensure_ascii=False)
