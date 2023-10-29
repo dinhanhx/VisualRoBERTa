9b9a7f2d0043b54aac0119478bc183e3539b1a7c
dinhanhx <dinhanhx@gmail.com>
2023-10-29 10:04:34 +0000

    Update hardcode path

--- kit/extract_lightning_ckpt.py
+++ kit/extract_lightning_ckpt.py
@@ -5,8 +5,8 @@ import torch
 from exp.run_pretrain import Wrapper
 
 if __name__ == "__main__":
-    ckpt_folder = Path("logs/lightning_logs/version_0/checkpoints")
-    ckpt_file = ckpt_folder.joinpath("epoch=1-step=18000.ckpt")
+    ckpt_folder = Path("pls-logs/lightning_logs/version_4/checkpoints/")
+    ckpt_file = ckpt_folder.joinpath("epoch=1-step=147000.ckpt")
     assert ckpt_file.is_file()
     wrapper = Wrapper.load_from_checkpoint(checkpoint_path=ckpt_file)
     pt_file = ckpt_folder.joinpath("imagetext-base.pt")
